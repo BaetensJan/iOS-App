24306304a5650d5ea97fb3cb9e74f7a3f6fe54d6
janbaetens <jan.baetens.w6130@student.hogent.be>
2018-11-20 16:02:04 +0100

    Initial Commit

---
+++ iRail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRail
+//
+//  Created by Jan Baetens on 20/11/2018.
+//  Copyright © 2018 Jan Baetens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
